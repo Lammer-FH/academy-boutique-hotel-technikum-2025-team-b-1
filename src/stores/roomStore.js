1a28d6bb6e1ed5124d54b65fa5fe834c93c06c05
Thomas  Bauer <thomasbauer@MacBook-Air-von-Thomas.local>
2025-11-20 12:31:12 +0100

    Ordnerstruktr angepasst und RoomComponenten eingebaut

---
+++ src/stores/roomStore.js
@@ -0,0 +1,24 @@
+import {PARAM_PARSER_BOOL as axios} from "vue-router/dist/experimental/index.mjs";
+import {defineStore} from "pinia";
+
+export const useRoomStore = defineStore("roomStore", {
+    actions: {
+        async checkAvailability(roomId,fromDate,toDate) {
+            try {
+                const response = await axios.get('https://boutique-hotel.helmuth-lammer.at/api/v1/room/${roomId}/from/${fromDate}/to/${toDate}');
+
+                if (response.status === 200) {
+                    return true;
+                }
+                return false;
+            }catch(err) {
+                if (err.response){
+                    if (err.response.status === 404) {
+                        return false;
+                    }
+                    throw new Error("Serverfehler: " + err)
+                }
+            }
+        }
+    }
+})
