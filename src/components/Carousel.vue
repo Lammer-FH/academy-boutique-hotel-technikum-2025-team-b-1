526ce20ba6af8a85fd25efafbe7f238a81668d0e
Theresa Divotgey <theresa.divotgey@hotmail.com>
2025-11-20 12:00:20 +0100

    update to bootstrap vue-next

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,35 +1,25 @@
 <template>
-  <div :id="carouselId" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
-      <div
-          v-for="(img, index) in images"
-          :key="index"
-          class="carousel-item"
-          :class="{ active: index === 0 }"
-      >
-        <img :src="img" class="d-block w-100" :alt="'Slide ' + index" />
-      </div>
-    </div>
-
-    <button class="carousel-control-prev" type="button" :data-bs-target="'#' + carouselId" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon"></span>
-    </button>
-    <button class="carousel-control-next" type="button" :data-bs-target="'#' + carouselId" data-bs-slide="next">
-      <span class="carousel-control-next-icon"></span>
-    </button>
-  </div>
+  <BCarousel
+      :id="carouselId"
+      controls
+      indicators
+      background="#000"
+      class="mb-4"
+  >
+    <BCarouselSlide
+        v-for="(img, index) in images"
+        :key="index"
+        :img-src="img"
+    />
+  </BCarousel>
 </template>
 
 <script setup>
+import {BCarousel,BCarouselSlide} from 'bootstrap-vue-next/components/BCarousel'
 defineProps({
   carouselId: { type: String, required: true },
   images: { type: Array, required: true }
 });
 </script>
 
-<style scoped>
-img {
-  height: 400px;
-  object-fit: cover;
-}
-</style>
+
